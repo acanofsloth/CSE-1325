a868de445b3b08f35587c7da29895c5bcddedbd0
acanofsloth <ivan.martinez5@mavs.uta.edu>
2021-09-06 16:59:15 -0500

    updated Roller.java file can now do basic dice rolls

--- P02/full_credit/Roller.java
+++ P02/full_credit/Roller.java
@@ -8,9 +8,15 @@ public class Roller
 		Scanner inputScan = new Scanner(System.in); // Create input scanner as an object
 		int numDices, facesPerDie;
 		System.out.print("Please enter number of Dices: ");
-		numDices = inputScam.nextLine(); 
+		numDices = inputScan.nextInt(); 
 		System.out.print("Please enter number of faces per Die: ");
-		facesPerDie = inputScam.nextLine(); 
-		int[] diceRolls = new int[numDices]
+		facesPerDie = inputScan.nextInt(); 
+		int[] diceRolls = new int[numDices];
+
+		for(int i = 0; i < numDices;i++)
+		{
+			diceRolls[i] = ((int)(100*Math.random()))/(100/facesPerDie);
+			System.out.println(diceRolls[i]);
+		}
     	}
 }
