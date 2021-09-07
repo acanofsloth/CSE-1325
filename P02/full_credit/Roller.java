2106251a71b9ceeb6ed5e91b2e4bd123f1281e8b
acanofsloth <ivan.martinez5@mavs.uta.edu>
2021-09-06 21:45:32 -0500

    Completed full_cred assignment

--- P02/full_credit/Roller.java
+++ P02/full_credit/Roller.java
@@ -1,22 +1,35 @@
-import java.util.Scanner; // Add input Scanner libraryq
-import java.util.Arrays;
+// Ivan Martinez
+// CSE-1325
+// 9/6/2021
+//
+
+import java.util.Arrays; // Add Arrays.sort
 
 public class Roller
 {
 	public static void main(String[] args) 
 	{
-		Scanner inputScan = new Scanner(System.in); // Create input scanner as an object
-		int numDices, facesPerDie;
-		System.out.print("Please enter number of Dices: ");
-		numDices = inputScan.nextInt(); 
-		System.out.print("Please enter number of faces per Die: ");
-		facesPerDie = inputScan.nextInt(); 
-		int[] diceRolls = new int[numDices];
+		// Declare Variables
+		if(args.length != 2) throw new IllegalArgumentException("usage: java Roll [#dice] [#sides]"); 
+		int numDices = Integer.parseInt(args[0]), facesPerDie = Integer.parseInt(args[1]), numSum = 0, numAverage = 0;
+		int[] diceRolls = new int[numDices]; // Create array sizeOf # of Dices 
 
-		for(int i = 0; i < numDices;i++)
+		// Add Rolls to array
+		for(int i = 0; i < numDices;i++) 
 		{
-			diceRolls[i] = ((int)(100*Math.random()))/(100/facesPerDie);
-			System.out.println(diceRolls[i]);
+			diceRolls[i] = (int)(100*Math.random())/(100/facesPerDie); // Rolled Dice = Generate dice roll from 0-100 / Probability to land per dice face
+			if(diceRolls[i] == facesPerDie){diceRolls[i]--;} // Incase math.random generates number 100, include to max dice face. 
+			numSum = numSum + diceRolls[i]; // Add for sum count
 		}
-    	}
+		
+		numAverage = numSum/numDices; // Calculate Average of all Dice Rolls
+		
+		// Organize and Print
+		Arrays.sort(diceRolls);
+		for(int i = 0; i < numDices;i++) 
+		{
+			System.out.print(diceRolls[i] + " ");
+		}
+		System.out.print(numSum + " " + numAverage);
+	}
 }
