56f9069c49fefc148d57e9dcbec4822d648abbdf
acanofsloth <ivan.martinez5@mavs.uta.edu>
2021-09-14 00:28:00 -0500

    added P03

---
+++ P03/full_credit/Grade.java
@@ -0,0 +1,14 @@
+// Ivan Martinez
+// CSE-1325
+// 9/7/2021
+//
+
+public class Grade
+{
+	public static void main(String[] args) 
+	{
+		
+
+		return 0;
+	}
+}
---
+++ P03/full_credit/Student.java
@@ -0,0 +1,37 @@
+// Ivan Martinez
+// CSE-1325
+// 9/7/2021
+//
+
+public class Student
+{
+
+	public Student()
+	{
+		return studentName();
+	}
+
+	public getName()
+	{
+
+	}
+
+	public addExam()
+	{
+
+	}
+
+	public average()
+	{
+
+	}
+	
+	public static void main(String[] args) 
+	{
+		
+	}
+
+	private String studentName();
+	private double examSum;
+	private double examNumGrades;
+}
